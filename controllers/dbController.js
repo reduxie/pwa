dffa654bd7501655d92562eaf35aaa341ec10e80
CBABY! <cbaby@CBABYs-MacBook-Pro.local>
2019-05-15 11:26:14 -0700

    moved models folder

--- controllers/dbController.js
+++ controllers/dbController.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const Sequelize = require('sequelize');
 const db = require('../config/database');
-const Job = require('../models/Job');
+const Job = require('../db/models/Job');
 
 router.get('/', (req, res) =>
   Job.findAll()
