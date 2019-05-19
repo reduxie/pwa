1ec0265b38236906a42293732c62192939daf2a6
Michael Mulderink <michaelmulderink@Michaels-MacBook-Pro.local>
2019-05-18 18:05:49 -0700

    ts server skelton built

---
+++ server/server.ts
@@ -0,0 +1,59 @@
+import express from 'express';
+
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const { PORT = 3000 } = process.env;
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(function (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET,HEAD,OPTIONS,POST,PUT,DELETE'
+  );
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  return next();
+});
+
+app.get('/searchImages', (req: express.Request, res: express.Response) => {
+  res.send('in   /searchImages');
+});
+
+app.get('/getDbImages', (req: express.Request, res: express.Response) => {
+  res.send('in   /getDbImages');
+});
+
+app.post(
+  '/addImageToFavsTable',
+  (req: express.Request, res: express.Response) => {
+    res.send('in   /addImageToFavsTable');
+  }
+);
+
+app.delete(
+  '/removeImageFromFavTable',
+  (req: express.Request, res: express.Response) => {
+    res.send('in   /removeImageFromFavTable');
+  }
+);
+
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server started at http://localhost:${PORT}`);
+  });
+}
