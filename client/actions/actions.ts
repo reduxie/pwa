60a8f82e6f2b405d3024ac35c96a4ded0f0476fb
Michael Mulderink <michaelmulderink@Michaels-MacBook-Pro.local>
2019-05-21 10:52:24 -0700

    I dont know what this is

--- client/actions/actions.ts
+++ client/actions/actions.ts
@@ -1,43 +1,51 @@
+import { Dispatch } from 'redux';
 import { actionTypes } from '../constants/actionTypes';
-import  { Dispatch } from 'redux';
 
 export const actions = {
-    saveSearch: (data: string) => ({type: actionTypes.SAVE_SEARCH, payload: data}),
-    // addSearch: (data: string) => ({type: actionTypes.ADD_SEARCH, payload: data}),
-    getSearchImage: (images: any[]) => ({type: actionTypes.GET_SEARCH_IMAGE, payload: images}),
-    getProfile: (images: any[]) => ({type: actionTypes.GET_PROFILE, payload: images})
-    // saveImage: (image: any) => ({actionTypes.SAVE_IMAGE, image}),
-}
+  saveSearch: (data: string) => ({
+    type: actionTypes.SAVE_SEARCH,
+    payload: data,
+  }),
+  // addSearch: (data: string) => ({type: actionTypes.ADD_SEARCH, payload: data}),
+  getSearchImage: (images: any[]) => ({
+    type: actionTypes.GET_SEARCH_IMAGE,
+    payload: images,
+  }),
+  getProfile: (images: any[]) => ({
+    type: actionTypes.GET_PROFILE,
+    payload: images,
+  }),
+  // saveImage: (image: any) => ({actionTypes.SAVE_IMAGE, image}),
+};
 
-export const getSearchImageAsync = (searchWord:string) => {
-    console.log('inside getsearchimageasync')
-    return function(dispatch: Dispatch) {
-        return fetch('http://localhost:3000/searchImages', {
-            method: "POST",
-            headers: {"content-type": "application/json"},
-            body: JSON.stringify({"image": searchWord})
-        })
-        .then(response => response.json())
-        .then(response => {
-            console.log("this is the response from server", response)
-            dispatch(actions.getSearchImage(response))
-        })
-    }
-}
+export const getSearchImageAsync = (searchWord: string) => {
+  console.log('inside getsearchimageasync');
+  return function(dispatch: Dispatch) {
+    return fetch('http://localhost:3000/searchImages', {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify({ image: searchWord }),
+    })
+      .then(response => response.json())
+      .then(response => {
+        console.log('this is the response from server', response);
+        dispatch(actions.getSearchImage(response));
+      });
+  };
+};
 
-export const getProfileAsync = (userId: number) => {
-    return function(dispatch: Dispatch) {
-        return fetch(`http://localhost:3000/getDbImages?userId=${userId}`, {
-            method: "GET",
-            headers: {"content-type": "application/json"},
-        })
-        .then(response => response.json())
-        .then(response => {
-            console.log("this is the response from server", response)
-            dispatch(actions.getProfile(response))
-        })
-    }
-}
+export const getProfileAsync = (userId: number) =>
+  function(dispatch: Dispatch) {
+    return fetch(`http://localhost:3000/getDbImages?userId=${userId}`, {
+      method: 'GET',
+      headers: { 'content-type': 'application/json' },
+    })
+      .then(response => response.json())
+      .then(response => {
+        console.log('this is the response from server', response);
+        dispatch(actions.getProfile(response));
+      });
+  };
 // export const getData = data => ({
 //   type: types.GET_DATA,
 //   payload: data,
