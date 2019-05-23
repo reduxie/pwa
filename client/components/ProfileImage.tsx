4cffe8c5ee4a9472b9ca0effc8d08de460ded9ba
Michael Mulderink <michaelmulderink@Michaels-MacBook-Pro.local>
2019-05-23 12:23:58 -0700

    copied modal into profileDisplay

--- client/components/ProfileImage.tsx
+++ client/components/ProfileImage.tsx
@@ -1,11 +1,43 @@
 import React from 'react';
 import DeleteIcon from '@material-ui/icons/Delete';
 // import Card from '@material-ui/core/Card';
+import { connect } from 'react-redux';
+import { actions } from '../actions/actions';
+
+const mapStateToProps = (store: any) => ({
+  bool: store.image.modalBool,
+  modalURL: store.image.modalURL,
+});
+
+const mapDispatchToProps = (dispatch: any) => ({
+  modalBool: (event: {}) => {
+    dispatch(actions.modalBool(event));
+  },
+});
 
 const ProfileImage = (props: any) => (
   <div className="display">
     {/* <Card> */}
-    <img src={props.image.img_url} className="displayImage" />
+    <div
+      onClick={e => {
+        if (props.bool === false) {
+          const changeObj: {} = {
+            modalURL: props.image.img_url,
+            modalBool: true,
+          };
+          props.modalBool(changeObj);
+        }
+        if (props.bool === true) {
+          const changeObj: {} = {
+            modalURL: '',
+            modalBool: false,
+          };
+          props.modalBool(changeObj);
+        }
+      }}
+    >
+      <img src={props.image.img_url} className="displayImage" />
+    </div>
     <DeleteIcon
       className="displayImage"
       onClick={event => props.deleteImage(props.image)}
@@ -14,4 +46,7 @@ const ProfileImage = (props: any) => (
     {/* <button onClick={(event) => props.deleteImage(props.image)}>DELETE</button> */}
   </div>
 );
-export default ProfileImage;
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ProfileImage);
