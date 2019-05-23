92235469d2b2d49be80748bf34dde63871a8fce0
Michael Mulderink <michaelmulderink@Michaels-MacBook-Pro.local>
2019-05-23 12:12:19 -0700

    created working modal on display.tsx

--- client/components/Display.tsx
+++ client/components/Display.tsx
@@ -1,20 +1,52 @@
 import React from 'react';
+import { connect } from 'react-redux';
+// import Card from '@material-ui/core/Card';
+// import { bool } from 'prop-types';
 import Image from './DisplayImage';
+import { actions } from '../actions/actions';
+
+const mapStateToProps = (store: any) => ({
+  bool: store.image.modalBool,
+  modalURL: store.image.modalURL,
+});
+
+const mapDispatchToProps = (dispatch: any) => ({
+  dropModal: () => {
+    dispatch(actions.dropModal());
+  },
+});
 
 interface IProps {
-    images: any[];
-    likedImage: any; 
+  images: any[];
+  likedImage: any;
 }
 
-const Display: React.FC<IProps> = (props) => {
-    const imageArr = []
-    for(let i = 0; i < props.images.length; i++) {
-        imageArr.push(<Image image={props.images[i]} likedImage={props.likedImage} />);
-    }
-    return (
-        <div className="display">
-            {imageArr}
-        </div>
-    ) 
-}
-export default Display;
+const Display: React.FC<IProps> = (props: any) => {
+  const imageArr = [];
+  for (let i = 0; i < props.images.length; i++) {
+    imageArr.push(
+      <Image image={props.images[i]} likedImage={props.likedImage} />
+    );
+  }
+  let modal;
+  // console.log()
+  if (props.bool) {
+    console.log('inside modal');
+    modal = (
+      <div id="modal" onClick={e => props.dropModal()}>
+        <img src={props.modalURL} />
+      </div>
+    );
+  }
+  return (
+    <div>
+      {modal}
+      <div className="display">{imageArr}</div>;
+    </div>
+  );
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Display);
--- client/components/ProfileImage.tsx
+++ client/components/ProfileImage.tsx
@@ -1,20 +1,17 @@
 import React from 'react';
 import DeleteIcon from '@material-ui/icons/Delete';
-import Card from '@material-ui/core/Card';
+// import Card from '@material-ui/core/Card';
 
-
-
-const ProfileImage = (props: any) => {
-    return (
-        <div className="display">
-            {/* <Card> */}
-                <img src={props.image.img_url} className="displayImage" />
-                <DeleteIcon className="displayImage" onClick={(event) => props.deleteImage(props.image)} />
-            {/* </Card> */}
-            {/* <button onClick={(event) => props.deleteImage(props.image)}>DELETE</button> */}
-        </div>
-    )
-}
+const ProfileImage = (props: any) => (
+  <div className="display">
+    {/* <Card> */}
+    <img src={props.image.img_url} className="displayImage" />
+    <DeleteIcon
+      className="displayImage"
+      onClick={event => props.deleteImage(props.image)}
+    />
+    {/* </Card> */}
+    {/* <button onClick={(event) => props.deleteImage(props.image)}>DELETE</button> */}
+  </div>
+);
 export default ProfileImage;
-
-
